e89dec305baffac147fc810c7116ed9c985776d9
Santi Miranda <santimir@protonmail.com>
2023-06-03 22:52:16 +0100

    chore: lint

--- src/__tests__/search.test.ts
+++ src/__tests__/search.test.ts
@@ -1,29 +1,29 @@
-import { join } from "node:path";
+import { join } from 'node:path';
 
-import { search } from "../search";
+import { search } from '../search';
 
-describe("search", () => {
-  const baseDir = join(__dirname, "data");
-  it("Return only files", async () => {
+describe('search', () => {
+  const baseDir = join(__dirname, 'data');
+  it('Return only files', async () => {
     const paths = await search(baseDir);
-    const expectedPaths = ["./a.txt", "./subdata/b.jpg"];
+    const expectedPaths = ['./a.txt', './subdata/b.jpg'];
     for (const ep of expectedPaths) {
       expect(paths).toContain(join(baseDir, ep));
     }
   });
-  it("Return only directories", async () => {
+  it('Return only directories', async () => {
     const paths = await search(baseDir, { files: false, directories: true });
-    const expectedPaths = ["./subdata", "./subdata/subdata2"];
+    const expectedPaths = ['./subdata', './subdata/subdata2'];
     for (const ep of expectedPaths) {
       expect(paths).toContain(join(baseDir, ep));
     }
   });
-  it("Return only jpg files", async () => {
+  it('Return only jpg files', async () => {
     const paths = await search(baseDir, {
-      filter: (name) => name.endsWith(".jpg"),
+      filter: (name) => name.endsWith('.jpg'),
     });
 
-    const expectedPath = "./subdata/b.jpg";
+    const expectedPath = './subdata/b.jpg';
     expect(paths).toContain(join(baseDir, expectedPath));
   });
 });
